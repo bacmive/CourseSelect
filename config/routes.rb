f52769b6c7659815024f26e21ea0807010865cee
bacmive <bacmive@sohu.com>
2019-10-22 19:19:59 +0800

    all

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :courses
+  resources :courses do
+    member do
+      get :select
+      get :quit
+      get :open
+      get :close
+    end
+    collection do
+      get :list
+    end
+  end
+
+  resources :grades, only: [:index, :update]
   resources :users
-  resources :grades
-  get  '/list_courses',   to: 'courses#list'
+
   get 'sessions/login' => 'sessions#new'
   post 'sessions/login' => 'sessions#create'
   delete 'sessions/logout' => 'sessions#destroy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+#-------------------------------------------------------------------------------------
+
+teacher_map={
+    35 => {name: "谭四君", department: "计算机与控制学院"}
+}
+
+course_map={
+    
+    35 => {course_code: "66666666H", name: "上天入地", course_type: "公共选修课", credit: "40/1.0", limit_num: 1, course_week: "第4-14周	", course_time: "周二(7-8)", class_room: "天宫", teaching_type: "课外讲授为主", exam_type: "课堂开卷", open: true},
+}
+
+teacher_map.keys.each do |index|
+  teacher=User.create!(
+      name: teacher_map[index][:name],
+      email: "teacher#{index}@test.com",
+      department: teacher_map[index][:department],
+      password: "password",
+      password_confirmation: "password",
+      teacher: true
+  )
+
+  teacher.teaching_courses.create!(
+      course_code: course_map[index][:course_code],
+      name: course_map[index][:name],
+      course_type: course_map[index][:course_type],
+      credit: course_map[index][:credit],
+      limit_num: course_map[index][:limit_num],
+      course_week: course_map[index][:course_week],
+      course_time: course_map[index][:course_time],
+      class_room: course_map[index][:class_room],
+      teaching_type: course_map[index][:teaching_type],
+      exam_type: course_map[index][:exam_type],
+      open: course_map[index][:open],
+  )
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
