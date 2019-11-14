1f2f910479df719e3c126171e56dfdfa5c8e8c48
bacmive <bacmive@sohu.com>
2019-11-14 19:54:51 +0800

    password reset v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'homes#index'
   resources :courses do
     member do
@@ -19,4 +23,5 @@ Rails.application.routes.draw do
   post 'sessions/login' => 'sessions#create'
   delete 'sessions/logout' => 'sessions#destroy'
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
