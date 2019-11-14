5a7823e1ae8218f0389e089678ea6b79e24cc44c
bacmive <bacmive@sohu.com>
2019-11-14 20:05:29 +0800

    v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token，:reset_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { email.downcase! }
   before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
