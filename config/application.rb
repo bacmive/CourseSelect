68be0c2d5da8105b824d5e21a7fa6562ae066c3f
bacmive <bacmive@sohu.com>
2019-11-29 17:10:10 +0800

    configure the rails_admin

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,10 @@ module HelloApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.i18n.default_locale = "zh-CN"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
