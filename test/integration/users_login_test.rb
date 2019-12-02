f5afecb96040432b7f341333d9ba8abb86003f79
bacmive <bacmive@sohu.com>
2019-12-02 16:12:15 +0800

    modify the UI “”“”

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,72 @@
+class UserLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:cheech)
+    @user1= users(:bacmive)
+    @user2= users(:archer)
+  end
+
+  test "login with valid information(as for admin)" do
+    get sessions_login_path
+    post sessions_login_path(params: {session: {email: @user.email, password: '111111'}})
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'homes/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", rails_admin_path, count: 1
+  end
+
+  test "login with valid information(as for non-admin)" do
+    get sessions_login_path
+    post sessions_login_path(params: {session: {email: @user1.email, password: '111111'}})
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'homes/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", rails_admin_path, count: 0
+  end
+
+  test "login with valid information(as for teacher)" do
+    get sessions_login_path
+    post sessions_login_path(params: {session: {email: @user2.email, password: '111111'}})
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'homes/index'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", rails_admin_path, count: 0
+  end
+
+  
+  test "login with valid information followed by logout" do
+    get sessions_login_path
+    post sessions_login_path, params: { session: {email: @user.email, password: '111111'}}
+    assert is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'homes/index'
+    assert_select "a[href=?]", sessions_login_path, count: 0
+    assert_select "a[href=?]", sessions_logout_path
+    assert_select "a[href=?]", edit_user_path(@user)
+    delete sessions_logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    #simulation: click the logout_link in another label page
+    delete sessions_logout_path
+    follow_redirect!
+    assert_select "a[href=?]", sessions_login_path
+    assert_select "a[href=?]", sessions_logout_path, count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,19 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+  
+  
+  class ActionDispatch::IntegrationTest
+  # 登入指定的用户
+    def log_in_as(user, password: '111111', remember_me: '1')
+      post sessions_login_path, params: { session: { email: user.email, password: password,remember_me: remember_me } }
+    end
+  end
 end
